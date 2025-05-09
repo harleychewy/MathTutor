e1005f576130464cf3fe2fb82d6e9d35d24ff933
harleychewy <lopeza4869@gmail.com>
2025-05-09 10:45:49 -0700

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Student1 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
